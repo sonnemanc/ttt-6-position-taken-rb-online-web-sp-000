7bbd4a36d01fe5bef92b85b32cdf175a9826295b
Corban <sonnemanc@gmail.com>
2021-06-28 18:51:30 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 def position_taken?(board, index)
   if board[0] == " " || board[0] == "" || board[0] == nil
     return false
+  else board[0] == "X" || board[0] == "O"
+    return true
   end
 end
